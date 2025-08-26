61e5465eef474ac9f840d647192e23d34e99aa3f
DanielMizaeldeMesquitaFerreira <danielmizaeldemf@gmail.com>
2025-08-26 17:21:44 -0300

    Update main.cpp

--- Teste/main.cpp
+++ Teste/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	float altura, Pesoideal, quilo, n;
-	int sexo;
-	std::cout << "Digite seu genero(feminino/masculino) : ";
-	std::cin >> sexo;
-	std::cout << "Digite sua altura :";
+
+float pesoideal(char genero, float altura)
+if( genero == 'm' ){
+	return 72.7 * altura - 58
+}
+else if( genero == 'f' )
+	return 62.1 * altura -58
+}
+
+main(){
+char r = 's' ;
+float altura;
+char genero;
+while( r == 's'){
+	std::cout << "Digite seu genero(m/f) :\n"
+	std::cin >> genero;
+	std::cout << "Digite sua altura :"
 	std::cin >> altura;
-	while(Pesoideal = quilo * altura - n){
+
+	std::cout << "Seu peso ideal e "<<pesoideal" em kg"
+	std::cout << "Deseja continuar ? (s/n) :"
+	std::cin >> r
+}
 	
-	
-	if(sexo == "feminino" ) {
-	
-	n = 58.0;
-	quilo = 72.7;
-	Pesoideal = quilo * altura - n
-	
-	std::cout << "Seu peso ideal e ",Pesoideal,"";
 	
 }
-	if(sexo = "masculino"){
-		Pesoideal = quilo * altura - n
-		quilo = 62.1;
-		n = 44.7;
+
 		
-		std::cout << "Seu peso  ideal e ",Pesoideal,"  "
-		
-	}
-}
 	return 0;
 }
